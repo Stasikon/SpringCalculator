71142f0a792271505ba75b56df142977267e3b11
Heppy <lecsa515@gmail.com>
2023-04-28 15:33:11 +0300

    Instal commit

---
+++ src/main/java/skyprospring/Calculaor/CalculaorApplication.java
@@ -0,0 +1,13 @@
+package skyprospring.Calculaor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculaorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculaorApplication.class, args);
+	}
+
+}
