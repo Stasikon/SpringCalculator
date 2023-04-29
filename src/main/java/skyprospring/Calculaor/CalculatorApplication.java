7513da49e472967aa783fcbbce06ac272cc3e4b2
Heppy <lecsa515@gmail.com>
2023-04-29 22:14:43 +0300

    Final commit

--- src/main/java/skyprospring/Calculaor/CalculatorApplication.java
+++ src/main/java/skyprospring/Calculaor/CalculatorApplication.java
@@ -3,12 +3,11 @@ package skyprospring.Calculaor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
-public class CalculaorApplication {
+public class CalculatorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CalculaorApplication.class, args);
+		SpringApplication.run(CalculatorApplication.class, args);
 	}
 
 }
